a9a4f34300ae9019179d0da042fde254bcf7e417
Gabriel do Prado Moreira <gabrieldopradomoreira@Gabriels-MacBook-Pro.local>
2026-04-07 12:25:58 -0300

    Initial project commit

---
+++ AssaAbloy/AssaAbloyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssaAbloyApp.swift
+//  AssaAbloy
+//
+//  Created by Gabriel do Prado Moreira on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AssaAbloyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
